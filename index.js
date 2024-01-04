de39bde118b4e4f78d6c6643a3b7ed1370344f37
ido gross <idogross1@gmail.com>
2024-01-04 16:19:09 +0200

    changed to telegraf

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
-const { addItemsToList, getCurrentList } = require('./models/lists-service.js');
-const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf, Scenes, session, Markup } = require('telegraf');
+const { addItemsToList,
+    getCurrentList,
+    deleteItemFromList,
+    getItems,
+    emptyList,
+    printItem
+} = require('./models/lists-service')
+
 require('dotenv').config()
 
-// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with the token you obtained from BotFather.
-const token = process.env.TOKEN;
-// Create a new Telegram bot instance
-const bot = new TelegramBot(token, { polling: true });
+const addItemsToListWizard = new Scenes.WizardScene(
+    'add-items-to-list-wizard',
+    ctx => {
+        ctx.reply('Enter items to list:');
+        ctx.wizard.state.data = {};
+        return ctx.wizard.next();
+    },
+    ctx => {
+        ctx.wizard.state.data.list = ctx.message.text;
+        addItemsToList(ctx.message);
+        return ctx.scene.leave();
+    }
+);
 
-// // Listen for incoming messages
-bot.on('message', addItemsToList);
+const stage = new Scenes.Stage([addItemsToListWizard]);
 
-bot.onText(/\/additemstolist/, (msg) => {
-    //   console.log("🚀 ~ bot.onText ~ msg:", msg);
-    console.log('1')
-    const chatId = msg.chat.id;
-    const message = msg.text;
-    console.log("🚀 ~ bot.onText ~ message:", message);
-    // Handle incoming messages here
-    // You can add your custom logic and reply to users.
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.use(session());
+
+bot.use(stage.middleware());
+
+bot.command('additemstolist', ctx => {
+    ctx.scene.enter('add-items-to-list-wizard');
 });
 
-bot.onText(/\/getCurrentList/, msg => {
-    console.log('2')
-    const currentList = getCurrentList();
-    bot.sendMessage(msg.chat.id, "Hello dear user");
+bot.command('getcurrentlist', ctx => {
+    ctx.reply(getCurrentList());
+});
+
+bot.command('editlist', ctx => {
+    const items = getItems();
+    // console.log("🚀 ~ items:", items);
+    ctx.reply('Items:');
+    items.forEach(({ item, quantity, comments }) => {
+        ctx.reply(printItem(item, quantity, comments), {
+            ...Markup.inlineKeyboard([
+                Markup.button.callback('delete item', 'delete-' + item)
+            ])
+        })
+    })
+});
+
+bot.action(/delete-(.*)/, ctx => {
+    const itemName = ctx.update.callback_query.data.match(/delete-(.*)/)[1];
+    // console.log("🚀 ~ bot.action ~ itemName:", itemName);
+    deleteItemFromList(itemName);
+    ctx.reply(getCurrentList());
 })
 
-bot.on('polling_error', err => {
-    console.log(err)
+bot.command("inline1", ctx => {
+    return ctx.reply("Coke or Pepsi?", {
+        ...Markup.inlineKeyboard([
+            Markup.button.callback("Coke", "Coke"),
+            Markup.button.callback("Pepsi", "Pepsi"),
+        ]),
+    });
+});
+
+bot.action('Coke', ctx => {
+    console.log('jjjjjjj')
 })
+
+bot.launch();
--- models/lists-service.js
+++ models/lists-service.js
@@ -3,7 +3,24 @@ const currentList = {
     id: 123,
     dateCreated: new Date(),
     dateUpdated: new Date(),
-    items: [],
+    items: [
+        {
+            item: 'one',
+            quantity: 33,
+            comments: 'kkkddd djdi sjij'
+        },
+        {
+            item: 'two',
+            comments: 'this is a comment'
+        },
+        {
+            item: 'three',
+            quantity: '9',
+        },
+        {
+            item: 'four'
+        }
+    ],
     current: true
 };
 
@@ -12,8 +29,15 @@ const emptyItem = {
 }
 
 function addItemsToList(message) {
-    console.log("🚀 ~ message:", message);
-    const items = message.text.split(/\n/);
+    console.log("🚀 ~ addItemsToList ~ message:", message);
+    const items = message.text.split(/\n/).map(item => {
+        const [name, quantity, comments] = item.split(',');
+        return {
+            item: name,
+            quantity,
+            comments
+        }
+    })
     currentList.items.push(...items);
     console.log('currentList:', JSON.stringify(currentList));
 }
@@ -22,11 +46,77 @@ function showLists(message) {
 
 }
 
-function getCurrentList() { 
-    return currentList.name;
+function getCurrentList(message) {
+    const string = currentList.items.map(({ item, quantity, comments }, i) => {
+        let str = item;
+
+        if (quantity || comments) {
+            str += ': '
+        }
+
+        if (quantity) {
+            str += quantity
+            if (comments) {
+                str += ', '
+            }
+        }
+
+        if (comments) {
+            str += comments;
+        }
+
+        if (i < currentList.items.length) {
+            str += '\n';
+        }
+        return str;
+    }).join('');
+    return string;
+}
+
+function printItem(item, quantity, comments) {
+    let str = item;
+
+    if (quantity || comments) {
+        str += ': '
+    }
+
+    if (quantity) {
+        str += quantity
+        if (comments) {
+            str += ', '
+        }
+    }
+
+    if (comments) {
+        str += comments;
+    }
+
+    return str;
+}
+
+function deleteItemFromList(itemToDelete) {
+    console.log("🚀 ~ deleteItemFromList ~ currentList.items:", currentList.items);
+    console.log("🚀 ~ deleteItemFromList ~ itemToDelete:", itemToDelete);
+    if (!currentList.items.length) return;
+    // const itemIndex = itemToDelete.match(/[0-9]+/g);
+    const itemIndex = currentList.items.findIndex(item => item.item === itemToDelete);
+    console.log("🚀 ~ deleteItemFromList ~ itemIndex:", itemIndex);
+    currentList.items.splice(itemIndex, 1);
+}
+
+function emptyList() {
+    currentList.items = [];
+}
+
+function getItems() {
+    return currentList.items;
 }
 
 module.exports = {
     addItemsToList,
     getCurrentList,
+    deleteItemFromList,
+    emptyList,
+    getItems,
+    printItem
 }
